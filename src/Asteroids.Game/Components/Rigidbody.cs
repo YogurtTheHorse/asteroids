9fd9519291999aa360035458f16d4b84949e17d3
YogurtTheHorse <yegorf1@gmail.com>
2020-11-21 21:13:27 +0300

    Add rigidbody data

--- src/Asteroids.Game/Components/Rigidbody.cs
+++ src/Asteroids.Game/Components/Rigidbody.cs
@@ -1,9 +1,19 @@
 using Asteroids.Core.Ecs;
+using Microsoft.Xna.Framework;
 
 namespace Asteroids.Systems.Game.Components
 {
     public class Rigidbody : Component
     {
+        public Vector2 Velocity { get; set; } = Vector2.Zero;
+
+        public float AngularVelocity { get; set; } = 0f;
         
+        public Vector2 Acceleration { get; set; } = Vector2.Zero;
+
+        public float AngularAcceleration { get; set; } = 0f;
+
+        public Rigidbody() => this
+            .Require<Transform>();
     }
 }
