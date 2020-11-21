d0d910d0f242b5c8763e0105d908ae0c2b995a6d
YogurtTheHorse <yegorf1@gmail.com>
2020-11-21 21:12:06 +0300

    Add exceptions

---
+++ src/Asteroids.Core.Tests/TestData/ComponentWithFooRequirement.cs
@@ -0,0 +1,9 @@
+﻿using Asteroids.Core.Ecs;
+
+namespace Asteroids.Core.Tests.TestData
+{
+    public class ComponentWithFooRequirement : Component
+    {
+        public ComponentWithFooRequirement() => Require<FooComponent>();
+    }
+}
---
+++ src/Asteroids.Core/Exceptions/ComponentNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Asteroids.Core.Exceptions
+{
+    public class ComponentNotFoundException : CoreException
+    {
+        public ComponentNotFoundException(Type componentType, int id) : base($"Entity doesn't have attached {componentType.Name}")
+        {
+            
+        }
+    }
+}
