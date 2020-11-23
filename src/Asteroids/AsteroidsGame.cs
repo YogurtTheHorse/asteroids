037924f348a8c724ae33894878b416d597fe7d3b
YogurtTheHorse <yegorf1@gmail.com>
2020-11-23 16:48:20 +0300

    Add fullscreen and N\M to scale screen

--- src/Asteroids/AsteroidsGame.cs
+++ src/Asteroids/AsteroidsGame.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using Asteroids.Core;
-using Asteroids.Systems.Game.Components;
 using Asteroids.Systems.Game.MessageHandlers;
 using Asteroids.Systems.Game.Messages;
 using Asteroids.Systems.Game.PolygonLoading;
 using Asteroids.Systems.Game.Systems;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using SharpMath2;
+using Microsoft.Xna.Framework.Input;
 
 namespace Asteroids
 {
@@ -16,11 +15,14 @@ namespace Asteroids
     /// </summary>
     public class AsteroidsGame : Game
     {
-        private PolygonLoader _polygonLoader;
-        private GraphicsDeviceManager _graphics;
+        private readonly PolygonLoader _polygonLoader;
+        private readonly GraphicsDeviceManager _graphics;
         private readonly World _world;
-        private RenderTarget2D _nativeRenderTarget;
-        private SpriteBatch _spriteBatch;
+        private RenderTarget2D _nativeRenderTarget = null!;
+        private SpriteBatch _spriteBatch = null!;
+
+        private int _scale = 2;
+        private bool _fullscreen = false;
 
         public AsteroidsGame()
         {
@@ -39,7 +41,7 @@ namespace Asteroids
             SpriteFont spriteFont = Content.Load<SpriteFont>("fonts/Eneminds Bold");
             SpriteFont vectorFont = Content.Load<SpriteFont>("fonts/VectorBattle");
 
-            SetScale(2);
+            SetScale(_scale);
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             _nativeRenderTarget = new RenderTarget2D(GraphicsDevice, _world.Width, _world.Height);
 
@@ -80,13 +82,54 @@ namespace Asteroids
                 .Register(new CollisionHandler(_world))
                 .Register(new SoundManager(Content))
                 .Register(new RendererSystemSwitcher(spriteFont, vectorFont, _world));
+
+            _world.Register<KeyPressed>(keyPressed =>
+            {
+                switch (keyPressed.Key)
+                {
+                    case Keys.N when _scale > 1:
+                        SetScale(_scale - 1);
+                        break;
+
+                    case Keys.M:
+                        SetScale(_scale + 1);
+                        break;
+
+                    case Keys.F:
+                        SetFullscreen(!_fullscreen);
+                        break;
+                }
+            });
         }
 
         private void SetScale(int scale)
         {
+            _scale = scale;
             _graphics.PreferredBackBufferWidth = _world.Width * scale;
             _graphics.PreferredBackBufferHeight = _world.Height * scale;
+            _graphics.IsFullScreen = false;
             _graphics.ApplyChanges();
+        }
+
+        private void SetFullscreen(bool fullscreen)
+        {
+            _fullscreen = fullscreen;
+            if (fullscreen)
+            {
+                _scale = Math.Min(
+                    GraphicsDevice.DisplayMode.Width / _world.Width,
+                    GraphicsDevice.DisplayMode.Height / _world.Height
+                );
+
+                _graphics.PreferredBackBufferWidth = GraphicsDevice.DisplayMode.Width;
+                _graphics.PreferredBackBufferHeight = GraphicsDevice.DisplayMode.Height;
+                _graphics.IsFullScreen = true;
+                _graphics.ApplyChanges();
+            }
+            else
+            {
+                SetScale(_scale);
+            }
         }
 
         protected override void Update(GameTime gameTime)
@@ -105,10 +148,25 @@ namespace Asteroids
 
             GraphicsDevice.SetRenderTarget(null);
 
-            _spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.None, RasterizerState.CullCounterClockwise);
+            _spriteBatch.Begin(
+                SpriteSortMode.Deferred,
+                BlendState.AlphaBlend,
+                SamplerState.PointClamp,
+                DepthStencilState.None,
+                RasterizerState.CullCounterClockwise
+            );
+
+            int width = _world.Width * _scale;
+            int height = _world.Height * _scale;
+
             _spriteBatch.Draw(
                 _nativeRenderTarget,
-                new Rectangle(0, 0, GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height),
+                new Rectangle(
+                    (GraphicsDevice.Viewport.Width - width) / 2,
+                    (GraphicsDevice.Viewport.Height - height) / 2,
+                    width,
+                    height
+                ),
                 Color.White
             );
             _spriteBatch.End();
